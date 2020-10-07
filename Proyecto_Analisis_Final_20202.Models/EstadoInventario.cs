378bbd56ede5ae0aa1567f30f34f1772ceb6e6d6
Maria Fernanda Sandi Calderon <504190867@ina.cr>
2020-10-06 19:53:51 -0600

    Espacios en enum

--- Proyecto_Analisis_Final_20202.Models/EstadoInventario.cs
+++ Proyecto_Analisis_Final_20202.Models/EstadoInventario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Proyecto_Analisis_Final_20202.Models
@@ -7,7 +9,11 @@ namespace Proyecto_Analisis_Final_20202.Models
     public enum EstadoInventario
     {
         Disponible = 1,
+
+        [Display(Name = "Sin existencias")]
         Sin_existencias = 2,
+
+        [Display(Name = "Fuera de inventario")]
         Fuera_de_Inventario =3 
 
     }
